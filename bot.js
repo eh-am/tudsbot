7e10e53c4de449a75cb199acadc51f40209d641a
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2016-05-28 00:39:18 -0400

    .

--- bot.js
+++ bot.js
@@ -27,6 +27,7 @@ module.exports = function(){
   var regex = '(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?';
 
   bot.onText(regex, function(msg){
+    console.log('recebi a seguinte mensagem', msg);
     msg.text.split(' ').forEach(function (word){
       // if it's a url
       if (validUrl.isValid(word)){
