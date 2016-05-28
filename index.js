f1e1b5264643bb88348194737c4ebcf02dab8272
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2016-05-27 23:23:13 -0400

    forgot to add status

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var express = require('express');
 var app = express();
 var port = (process.env.PORT || 3000);
 
-require('./bot.js');
+require('./bot.js')();
 
 app.get('/', function(req, res){
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({name: 'tudsbot', ver: '0.0.1'}));
+  res
+    .status(200);
+    .setHeader('Content-Type', 'application/json');
+    .send(JSON.stringify({name: 'tudsbot', ver: '0.0.1'}));
 });
 
 app.listen(port, function(){
