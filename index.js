453c7021681aed05b4591c6b476972fa8002f75a
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2016-06-06 09:05:54 -0400

    .

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.get('/', function(req, res){
   res.send(JSON.stringify({name: 'tudsbot', ver: '0.0.1'}));
 });
 
+app.get('wakemydyno.txt', function(req, res){
+  res.status(200);
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify({name: 'tudsbot', ver: '0.0.1'}));
+});
+
 app.listen(port, function(){
   console.log('Listening on port ' + port);
 });
