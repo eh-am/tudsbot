272fc6a16afd405e9c65a2790f16af6d47e60d2f
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2016-06-05 22:32:11 -0400

    .

--- bot.js
+++ bot.js
@@ -25,8 +25,18 @@ module.exports = function(){
 
   // this regex is supposed to match positively if a string contails an url somewhere
   var regex = new RegExp('(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?');
+  var alcunhas = ['tudsbot', 'pardo', 'moreninho'];
 
   bot.on('message', function(msg){
+    if (msg.chat.type === "private"){
+      bot.sendMessage(msg.chat.id, "Só funciono em grupos, vá caçar uns amigos");
+      return ;
+    }
+
+    if (new RegExp('^(' +  alcunhas.join('|') + ')$', 'i').test(msg.text)){
+      bot.sendMessage(msg.chat.id, "Chamou?", {reply_to_message_id: msg.message_id });
+    }
+
 
     if (new RegExp('tudsbot apagar \\d+', 'i').test(msg.text)) {
       var deleteId = msg.text.match('tudsbot apagar (\\d+)', 'i')[1];
