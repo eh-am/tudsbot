5c665684fba9d8f361df7795f2f85ef4510fcf49
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2016-06-15 16:23:11 -0400

    .

--- src/features/main.js
+++ src/features/main.js
@@ -3,10 +3,13 @@
 var sera = require('./sera');
 var chamou = require('./chamou');
 var conselhos = require('./conselhos');
+var evc = require('./e-vc');
 
 module.exports = {
   attachAll: function(bot){
     sera.attach(bot);
     chamou.attach(bot);
+    evc.attach(bot);
+    conselhos.attach(bot);
   }
 };
